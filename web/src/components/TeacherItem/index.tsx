8119378f4fdab7d4c56f266e8736fc9ecf762c43
Leonardo <leonandrade22@gmail.com>
2020-10-19 10:01:15 -0300

    1.0
    feat: app last version

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,33 +3,50 @@ import React from 'react';
 import whatsappIcon from '../../assets/img/icons/whatsapp.svg';
 
 import './styles.css';
+import api from "../../services/api";
 
-function TeacherItem() {
-  return (
+export interface Teacher{
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
+
+    return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars0.githubusercontent.com/u/25047795?s=460&u=76a5f6ac344e485848c1f89cba2ccfc2887e6ed5&v=4" alt="Projeto React" />
+        <img src={teacher.avatar} alt="Projeto React" />
         <div>
-          <strong>Leonardo Andrade</strong>
-          <span>Programação Web</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        O React é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web.
-            <br /><br />
-            É mantido pelo Facebook, Instagram, outras empresas e uma comunidade de desenvolvedores individuais.
-          </p>
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-              <strong>R$ 95,00</strong>
+              <strong>R$ {teacher.cost},00</strong>
         </p>
-        <button type="button">
+        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="whatsapp" />
-              Entrar em contato
-            </button>
+          Entrar em contato
+        </a>
       </footer>
     </article>
   );
